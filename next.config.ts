4220bbe8cedc056ca4d602f1357ae08731cdbb4c
melissa monteiro santana <melissamonteirosant@gmail.com>
2026-08-26 14:40:06 -0400

    Add complete portfolio

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
 
-const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/Portifolio",
+
+  images: {
+    unoptimized: true,
+  },
+
+  allowedDevOrigins: ["192.168.0.241"],
 };
 
-export default nextConfig;
+module.exports = nextConfig;
