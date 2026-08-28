22adee89b9d3d6412390659ea20c51d583129fe8
Melissa Monteiro Santana <melissamonteirosant@gmail.com>
2026-08-28 16:55:52 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-
-  basePath: "/Portifolio",
-
-  images: {
-    unoptimized: true,
-  },
-
-  allowedDevOrigins: ["192.168.0.241"],
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
